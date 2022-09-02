02fafd0c2035c141669df0e53f450e35201f19b2
Spencer <spencerb242@proton.me>
2022-09-01 20:59:34 -0600

    created new files iss_promised.js and index2.js that implement promises to handle async.

---
+++ iss_promised.js
@@ -0,0 +1,27 @@
+const request = require('request-promise-native');
+
+const fetchMyIP = () => {
+  return request('https://api.ipify.org?format=json');
+};
+
+const fetchCoordsByIP = (body) => {
+  const ip = JSON.parse(body).ip;
+  return request('http://ipwho.is/' + ip);
+}
+
+const fetchISSFlyOverTimes = (body) => {
+  const { latitude, longitude } = JSON.parse(body);
+  return request(`https://iss-pass.herokuapp.com/json/?lat=${latitude}&lon=${longitude}`)
+}
+
+const nextISSTimesForMyLocation =  () => {
+  return fetchMyIP()
+          .then(fetchCoordsByIP)
+          .then(fetchISSFlyOverTimes)
+          .then((data) => {
+            const { response } = JSON.parse(data);
+            return response;
+          });
+}
+
+module.exports = { nextISSTimesForMyLocation };
